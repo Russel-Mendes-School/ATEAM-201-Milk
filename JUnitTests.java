c04e30db7e1d9bb4d10625df1a7d189e16400d55
Will Hu <willhu@Wills-MacBook-Pro.local>
2020-04-14 18:27:12 -0500

    Added the JUnit test class to the repo

---
+++ JUnitTests.java
@@ -0,0 +1,66 @@
+//ATeam7
+//Russel Mendes ​rmendes@wisc.edu​, Lec 002, xTeam 201
+//Akshay Bodla ​bodla@wisc.edu​, Lec 001, xTeam 138
+//Roland Jiang ​rjiang45@wisc.edu​, Lec 002, xTeam 201
+//Will Hu, ​whu72@wisc.edu​ , Lec 002, xTeam 201
+//Ben Rinvelt, ​Rinvelt@wisc.edu​, Lec 002, xTeam 241
+//JUnitTests Class - tests the functionality of various 
+//classes in our ATeam program
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/** TODO: add class header comments here*/
+public class JUnitTests{
+
+    // TODO: add other fields that will be used by multiple tests
+    
+    // TODO: add code that runs before each test method
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    // TODO: add code that runs after each test method
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    
+    /** 
+     * Tests that the farm class works as intended
+     *	
+     */
+    @Test
+    public void test000_testFarmClass() {
+    	
+    }
+        
+    /** 
+     * Tests that a Graph returns all the packages properly
+     */
+    @Test
+    public void test001_testMonthClass() {
+        
+    }
+    /**
+     * Tests that a Graph removes a vertex properly
+     */
+    @Test
+    public void test002_testFileManagerClass()
+    {
+
+    }
+
+    
+    // TODO Add more tests as we see fit
+    
+}
+
